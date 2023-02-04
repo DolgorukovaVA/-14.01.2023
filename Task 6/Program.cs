be56b584d28189fdb3c9768450f2705a151a18ef
Valentina <west.77@mail.ru>
2023-02-04 13:51:56 +0300

    Task 6 added

---
+++ Task 6/Program.cs
@@ -0,0 +1,8 @@
+﻿// ЗАДАНИЕ 6: Показать вторую цифру трёхзначного числа
+
+Console.WriteLine();
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
+Console.WriteLine(); 
