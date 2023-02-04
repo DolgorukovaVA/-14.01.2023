9c7798a6826f6eb6fb681f369c9d94ea32dcab89
Valentina <west.77@mail.ru>
2023-02-04 21:22:26 +0300

    Task 7 added

---
+++ Task 7/Program.cs
@@ -0,0 +1,28 @@
+﻿// ЗАДАНИЕ 7:Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+//Способ 1
+
+Console.WriteLine("Введите число из отрезка [10,99]:");
+int number = Int32.Parse(Console.ReadLine());
+int numberA = number % 10;
+int numberB = number / 10;
+
+if(numberA > numberB) Console.WriteLine("наибольшая цифра = " + numberA);
+
+if(numberA == numberB) Console.WriteLine("наибольшей цифры нет");
+
+if(numberA < numberB) Console.WriteLine("наибольшая цифра = " + numberB);
+
+
+
+
+//Способ 2
+
+//int number = new Random().Next(9,100);
+//Console.WriteLine(number);
+//int numberA = number % 10;
+//int numberB = number / 10;
+//if(numberA > numberB) Console.WriteLine("наибольшая цифра = " + numberA);
+//if(numberA == numberB) Console.WriteLine("наибольшей цифры нет");
+//if(numberA < numberB) Console.WriteLine("наибольшая цифра = " + numberB);
+
