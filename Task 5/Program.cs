83842355597e0a875cb24d5e1b98cdd847dc0402
Valentina <west.77@mail.ru>
2023-02-04 13:48:07 +0300

    Task 5 added

---
+++ Task 5/Program.cs
@@ -0,0 +1,8 @@
+﻿// ЗАДАНИЕ 5: Показать последнюю цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
+Console.WriteLine();
+
+
