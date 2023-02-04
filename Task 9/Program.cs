1ad14242e6d3e299d772fcb27717288e14bd708b
Valentina <west.77@mail.ru>
2023-02-05 01:08:29 +0300

    Task 9 added

---
+++ Task 9/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЗАДАНИЕ 9: Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Введите первое число:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if(Number1 % Number2 == 0) Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+
+else
+{
+    Console.WriteLine($"Число {Number1} не кратно числу {Number2}, остаток от деления равен {Number1 % Number2}");
+}
+Console.WriteLine();
