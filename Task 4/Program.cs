883a74cb0f3c06daec8e1a567c6ed6691fca484d
Valentina <west.77@mail.ru>
2023-02-04 13:39:51 +0300

    Task 4 added

---
+++ Task 4/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАНИЕ 4: Показать четные числа от 1 до N
+Console.Write("Введите число:   ");
+int endNum = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+Console.WriteLine($"Чётные числа из диапазона от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if ((num % 2) == 0) Console.Write($"{num} ");
+}
+Console.WriteLine();
